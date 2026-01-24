729493f1dbea0eb5b1f1a01259136149f4e36a49
Phúc Trung Bùi <trungbp@Phucs-Mac-mini.local>
2026-01-24 08:45:26 +0700

    replace ghostty with wezterm

---
+++ roles/wezterm/files/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require("wezterm")
+
+return {
+	-- Font
+	font_size = 16.0,
+
+	-- Color scheme
+	color_scheme = "Dracula",
+
+	-- Tab bar
+	enable_tab_bar = true,
+	tab_bar_at_bottom = true,
+	hide_tab_bar_if_only_one_tab = false,
+
+	-- macOS: hide window title bar but keep resize controls
+	window_decorations = "RESIZE",
+}
