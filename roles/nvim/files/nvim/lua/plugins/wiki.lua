b6ef369c1135998d900cdeb15a20885607187106
Trung Bui <trubui@axon.com>
2026-04-15 00:01:59 +0700

    remove nb

--- roles/nvim/files/nvim/lua/plugins/wiki.lua
+++ roles/nvim/files/nvim/lua/plugins/wiki.lua
@@ -3,7 +3,7 @@ return {
     "lervag/wiki.vim",
     lazy = false,
     init = function()
-      vim.g.wiki_root = "~/wiki"
+      vim.g.wiki_root = vim.fn.expand("~/wiki")
       vim.g.wiki_filetypes = { "md" }
       vim.g.wiki_link_target_type = "md"
       vim.g.wiki_mappings_global = {
