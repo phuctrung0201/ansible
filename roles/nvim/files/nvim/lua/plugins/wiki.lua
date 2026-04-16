6be5b123598f1d612ff0e74e3dfb9ae23fa9bf6e
Trung Bui <trubui@axon.com>
2026-04-16 16:34:51 +0700

    wiki

--- roles/nvim/files/nvim/lua/plugins/wiki.lua
+++ roles/nvim/files/nvim/lua/plugins/wiki.lua
@@ -2,13 +2,32 @@ return {
   {
     "lervag/wiki.vim",
     lazy = false,
+    keys = {
+      { "<leader>wj", "<cmd>WikiJournal<cr>", desc = "Journal today" },
+      { "<leader>wi", "<cmd>WikiIndex<cr>", desc = "Wiki index" },
+      { "<leader>wp", "<cmd>WikiPages<cr>", desc = "Wiki pages" },
+    },
     init = function()
+      vim.g.wiki_mappings_use_defaults = 0
       vim.g.wiki_root = vim.fn.expand("~/wiki")
       vim.g.wiki_filetypes = { "md" }
       vim.g.wiki_link_target_type = "md"
-      vim.g.wiki_mappings_global = {
-        ["<plug>(wiki-pages)"] = "<leader>wp",
-      }
+    end,
+    config = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "WikiBufferInitialized",
+        callback = function()
+          local map = function(lhs, rhs, desc)
+            vim.keymap.set("n", lhs, rhs, { buffer = true, desc = desc })
+          end
+          map("<cr>", "<cmd>WikiLinkFollow<cr>", "Follow link")
+          map("<bs>", "<cmd>WikiLinkReturn<cr>", "Return from link")
+          map("<leader>wa", "<cmd>WikiLinkAdd<cr>", "Wiki: insert link")
+          map("<leader>wt", "<cmd>WikiLinkTransform<cr>", "Wiki: transform link")
+          map("<leader>wG", "<cmd>WikiTocGenerate<cr>", "Wiki: generate TOC")
+          map("<leader>wg", "<cmd>WikiTocGenerateLocal<cr>", "Wiki: generate local TOC")
+        end,
+      })
     end,
   },
 }
