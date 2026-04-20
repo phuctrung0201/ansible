25bc0df6a9c27eb8125c1880c7a8bd1546a099d1
Trung Bui <trubui@axon.com>
2026-04-20 17:55:36 +0700

    sql lua

--- roles/nvim/files/nvim/lua/plugins/sql.lua
+++ roles/nvim/files/nvim/lua/plugins/sql.lua
@@ -11,7 +11,48 @@ return {
   {
     "kristijanhusak/vim-dadbod-ui",
     keys = {
-      { "<leader>D", "<cmd>DBUIToggle<cr>", desc = "Toggle DBUI" },
+      { "<leader>D", false },
+      { "<leader>dd", "<cmd>DBUIToggle<CR>", desc = "Toggle DB UI" },
+    },
+    init = function()
+      vim.g.db_ui_use_nvim_notify = true
+
+      vim.api.nvim_create_autocmd("BufWinEnter", {
+        callback = function()
+          vim.schedule(function()
+            if vim.b.dbui_db_key_name then
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                if vim.bo[vim.api.nvim_win_get_buf(win)].filetype == "snacks_dashboard" then
+                  vim.api.nvim_win_close(win, false)
+                  break
+                end
+              end
+            end
+          end)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "sql", "mysql", "plsql" },
+        callback = function(ev)
+          local map = function(l, r, desc)
+            vim.keymap.set("n", l, r, { buffer = ev.buf, desc = desc })
+          end
+          map("<leader>ds", "<Plug>(DBUI_ExecuteQuery)", "Execute query")
+          map("<leader>dS", "<Plug>(DBUI_SaveQuery)", "Save query")
+          map("<leader>de", "<Plug>(DBUI_EditBindParameters)", "Execute with bind params")
+        end,
+      })
+    end,
+  },
+
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>d", group = "database" },
+        { "<leader>D", group = "debug" },
+      },
     },
   },
 }
