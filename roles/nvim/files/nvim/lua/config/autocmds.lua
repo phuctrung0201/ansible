279d48ad864d9a48af07a12583d6c9a2a5a3076c
Trung Bui <trubui@axon.com>
2026-04-29 17:18:09 +0700

    add project handle

--- roles/nvim/files/nvim/lua/config/autocmds.lua
+++ roles/nvim/files/nvim/lua/config/autocmds.lua
@@ -11,3 +11,8 @@ autocmd("TermOpen", {
   group = augroup("term_scrollback", { clear = true }),
   callback = function() vim.opt_local.scrollback = 1000 end,
 })
+
+autocmd("DirChanged", {
+  group = augroup("bufferline_refresh", { clear = true }),
+  callback = function() vim.cmd("redrawtabline") end,
+})
