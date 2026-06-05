3ab157f18ae273ec06af96bdb6fbcc3289c94ff1
Trung Bui <trubui@axon.com>
2026-06-05 20:01:15 +0700

    add zig

--- roles/nvim/files/nvim/lua/config/lazy.lua
+++ roles/nvim/files/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.zig" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "plugins" },
--- roles/nvim/files/nvim/lua/plugins/lsp.lua
+++ roles/nvim/files/nvim/lua/plugins/lsp.lua
@@ -28,6 +28,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = { enabled = false },
       servers = {
         bashls = {
           filetypes = { "sh", "zsh", "bash" },
