bca1a11ea7c336094b1e0903924fba0dc120a2a6
Trung Bui <trubui@axon.com>
2026-07-09 10:12:27 +0700

    simplify obsidian setup

--- roles/nvim/files/nvim/lua/plugins/obsidian.lua
+++ roles/nvim/files/nvim/lua/plugins/obsidian.lua
@@ -20,85 +20,28 @@ return {
       { "<leader>oe", "<cmd>Obsidian template<cr>", desc = "Obsidian: insert template" },
     },
     opts = function()
-      local builtin = require("obsidian.builtin")
-      local vault = vim.env.OBSIDIAN_VAULT_DIR or "~/wiki"
-
       return {
         legacy_commands = false,
         workspaces = {
-          { name = "wiki", path = vault },
+          { name = "wiki", path = "~/wiki" },
         },
         notes_subdir = "notes",
         new_notes_location = "notes_subdir",
-        note_id_func = builtin.title_id,
-        sort_by = "modified",
-        sort_reversed = true,
-
-        note = {
-          template = "note.md",
-        },
-
+        note_id_func = require("obsidian.builtin").title_id,
+        note = { template = "note.md" },
         daily_notes = {
           folder = "journals",
           date_format = "%Y-%m-%d",
           alias_format = "%A, %B %-d %Y",
           template = "journal.md",
         },
-
         templates = {
           folder = ".templates",
           date_format = "%A, %B %-d %Y",
           time_format = "%H:%M",
         },
-
-        completion = { min_chars = 2 },
         picker = { name = "snacks.pick" },
-
-        link = {
-          style = "wiki",
-        },
-
-        frontmatter = {
-          enabled = true,
-          func = function(note)
-            local out = { tags = note.tags or {} }
-            if note.metadata then
-              for k, v in pairs(note.metadata) do
-                out[k] = v
-              end
-            end
-            return out
-          end,
-          sort = { "tags" },
-        },
-
-        mappings = {
-          ["gf"] = {
-            action = function()
-              return require("obsidian").util.gf_passthrough()
-            end,
-            opts = { noremap = false, expr = true, buffer = true },
-          },
-          ["<cr>"] = {
-            action = function()
-              return require("obsidian").util.smart_action()
-            end,
-            opts = { buffer = true, expr = true },
-          },
-          ["<leader>ch"] = {
-            action = function()
-              return require("obsidian").util.toggle_checkbox()
-            end,
-            opts = { buffer = true },
-          },
-        },
-
-        follow_url_func = vim.ui.open,
-        follow_img_func = function(img)
-          vim.fn.jobstart({ "qlmanage", "-p", img })
-        end,
-
-        ui = { enable = true },
+        frontmatter = { enabled = false },
       }
     end,
   },
